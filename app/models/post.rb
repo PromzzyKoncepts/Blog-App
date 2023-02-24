07e9b356a38c9be245bcb007c37155ba96f529a4
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-24 17:17:27 +0100

    Instal Rspec to tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence:true,length:{maximum: 250}
+  validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
+
+
   def self.update(id)
     User.find(id).increment!(:posts_counter)
-  end
-
-  def recent_comments(limit = 5)
-    Comment.order(created_at: :desc).limit(limit)
   end
 
   def last_five_comments
