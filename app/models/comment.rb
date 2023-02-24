692bd65b57c63281ddb034851d7494e89250f459
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-24 19:42:23 +0100

    edit file to correct methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update(id)
-    Post.find(id).increment!(:comments_counter)
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
