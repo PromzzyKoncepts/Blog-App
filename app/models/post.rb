a71c3bc651b5340481e75070bb44e8fcef33d1fd
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-24 13:23:37 +0100

    Correct based on review

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
   def self.update(id)
     User.find(id).increment!(:posts_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def returns_last_three_post
     posts.last(3)
