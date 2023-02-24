7f281999688b4bc5c9bc17be27c1b8ea47c3c5d7
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-02-24 10:59:20 +0100

    Add model and methods for post

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+
+  def self.update(id)
+    User.find(id).increment!(:posts_counter)
+  end
+
+  def recent_comments(limit = 5)
+    Comment.order(created_at: :desc).limit(limit)
+    
+  end
+
+   def last_five_comments
+    Comment.last(5)
+  end
+end
