ff2a9a25e4b60bf48b13a56eae57570719a999b6
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-01 18:00:23 +0100

    rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def show
-  end
+  def show; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def show
-  end
+  def show; end
 end
