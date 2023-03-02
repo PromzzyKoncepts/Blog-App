7f7ae954f6352e2bf3f2db9fa981ee797e16bfb0
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-02 09:18:47 +0100

    make changes based on review

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
+  root 'users#index' 
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
