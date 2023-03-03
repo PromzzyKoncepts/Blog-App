2b29bdb544145d645bad13dbe01cb8139c29f70d
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-03 14:44:05 +0100

    add default: 0 to file

--- db/migrate/20230302223014_create_user.rb
+++ db/migrate/20230302223014_create_user.rb
@@ -4,8 +4,8 @@ class CreateUser < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :photo, default: 'https://plus.unsplash.com/premium_photo-1666262369867-6e521a979afb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
       t.text :bio
-      t.integer :posts_counter
-      
+      t.integer :posts_counter, default: 0
+
       t.timestamps
     end
   end
