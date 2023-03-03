57c0014bd38ad60979b34b6dad9a5cf54f3c449e
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-03 18:41:06 +0100

    fix rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: @user.id)
-    @post = Post.find(params[:id]) 
+    @post = Post.find(params[:id])
   end
 end
