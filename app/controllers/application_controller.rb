f14e2ba94289ff1526fd3cda57efa26d03073692
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-04 16:54:38 +0100

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base  
+class ApplicationController < ActionController::Base
   def current_user
     User.first
   end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,10 +4,7 @@ class LikesController < ApplicationController
     @like = Like.new(author_id: current_user.id, post_id: @post.id)
 
     return unless @like.save
+
     redirect_to user_post_path(@like.author_id, @like.post_id)
   end
-
-  # def like_params
-  #   { author: current_user, post_id: params.require(:post_id) }
-  # end
 end
