5371913170bd8ede4035bdb9e0eb1cf3a2a7f985
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-06 17:13:25 +0100

    Write integration test for userindex  views

---
+++ spec/requests/views/user_index_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Render the page", type: :feature do
+  before(:all) do
+    @first_user = User.create(name: 'Promise Okechukwu', photo: 'https://avatars.githubusercontent.com/u/101924220?v=4', bio: 'sample for test')
+
+    post1 = Post.create(title: 'Rspec test', text: 'sample text for bio', author_id: @first_user)
+
+    it 'shows the username of all users' do
+      visit users_path
+      expect(page).to have_content(@first_user.name)
+    end
+  end
+end
