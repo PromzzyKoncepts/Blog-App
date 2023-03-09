3cfe53e12c4a4ab6cdd3ab032f13db3201f9e986
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-09 08:21:36 +0100

    Add devise to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true, length: { in: 1..250 }
+  validates :name, presence: true
+  validates :email, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def returns_last_three_post
